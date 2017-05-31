9baba058e6eb2729a5f0b82dfbed7e35d458412c
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2017-05-31 11:08:07 +0300

    Some cleaning in the visit helper

--- PatientManagement/PatientManagement.Web/Modules/Common/Helpers/VisitsNotificationHelpers.cs
+++ PatientManagement/PatientManagement.Web/Modules/Common/Helpers/VisitsNotificationHelpers.cs
@@ -24,23 +24,24 @@ namespace PatientManagement.Web.Modules.Common.Helpers
             var patientName = connection.First<PatientsRow>(new Criteria(PatientsRow.Fields.PatientId) == patientId.ToString()).Name;
             var users = connection.List<UserRow>().Where(e => e.TenantId == user.TenantId && e.UserId != Int32.Parse(user.Id)).Select(e => e.UserId.ToString());
 
+            var notification = string.Empty;
             switch (status)
             {
                 case EVisitNotificationStatus.Created:
-                    var notification = string.Format(Texts.Site.Notifications.VisitAddedNotification, user.DisplayName, patientName);
-                    notificationHub.Clients.Users(users.ToList()).visitChangedNotification(notification, start, end);
+                    notification = string.Format(Texts.Site.Notifications.VisitAddedNotification, user.DisplayName, patientName);
                     break;
                 case EVisitNotificationStatus.Updated:
                     notification = string.Format(Texts.Site.Notifications.VisitChangedNotification, user.DisplayName, patientName);
-                    notificationHub.Clients.Users(users.ToList()).visitChangedNotification(notification, start, end);
                     break;
                 case EVisitNotificationStatus.Deleted:
                     notification = string.Format(Texts.Site.Notifications.VisitDeletedNotification, user.DisplayName, patientName);
-                    notificationHub.Clients.Users(users.ToList()).visitChangedNotification(notification, start, end);
                     break;
 
                 default: break;
             }
+
+            notificationHub.Clients.Users(users.ToList()).visitChangedNotification(notification, start, end);
+
         }
     }
 
