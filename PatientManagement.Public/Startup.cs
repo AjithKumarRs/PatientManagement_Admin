f54ca54787eca71ecb6e0b8d7f1ed1a8b7a0c2dd
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2017-06-27 22:14:37 +0300

    fixed email contact form

--- PatientManagement.Public/Startup.cs
+++ PatientManagement.Public/Startup.cs
@@ -18,6 +18,7 @@ namespace PatientManagement.Public
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,7 +64,7 @@ namespace PatientManagement.Public
             };
 
             app.UseStaticFiles(saticFileOptions);
-
+            app.UseMvc();
             app.Use(async (context, next) =>
             {
                 if (context != null)
