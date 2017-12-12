7752d7c8252fdc53589e0ddd78237294dd9ec0ee
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2017-12-12 10:40:55 +0200

    Added new components for small footer, large footer, main menu and changed favicon

---
+++ PatientManagement.Reservation/PatientManagement.Reservation.Web.Mvc/Views/Shared/Components/FooterSmallBar/FooterSmallBarViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Abp.Configuration.Startup;
+using PatientManagement.Reservation.Sessions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PatientManagement.Reservation.Web.Views.Shared.Components.FooterSmallBar
+{
+    public class FooterSmallBarViewComponent : ReservationViewComponent
+    {
+        private readonly ISessionAppService _sessionAppService;
+        private readonly IMultiTenancyConfig _multiTenancyConfig;
+
+        public FooterSmallBarViewComponent(ISessionAppService sessionAppService,
+            IMultiTenancyConfig multiTenancyConfig)
+        {
+            _sessionAppService = sessionAppService;
+            _multiTenancyConfig = multiTenancyConfig;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ PatientManagement.Reservation/PatientManagement.Reservation.Web.Mvc/Views/Shared/Components/MainTopBarNav/MainTopBarNavViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Abp.Application.Navigation;
+
+namespace PatientManagement.Reservation.Web.Views.Shared.Components.MainTopBarNav
+{
+    public class MainTopBarNavViewModel
+    {
+        public UserMenu MainMenu { get; set; }
+
+        public string ActiveMenuItemName { get; set; }
+    }
+}
